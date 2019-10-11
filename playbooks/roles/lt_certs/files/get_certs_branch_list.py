aa5ef773b61610175a92bde126be457ab6a24e9d
yves <y.shen@learning-tribes.com>
2019-10-11 10:41:22 +0800

    set certsificate branch name on stage

---
+++ playbooks/roles/lt_certs/files/get_certs_branch_list.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+
+
+username = os.environ['GITHUB_USERNAME']
+password = os.environ['GITHUB_PASSWORD']
+
+payload = {'per_page': '999'}
+r = requests.get('https://api.github.com/repos/Learningtribes/edx-certificates/branches', auth=(username, password), params=payload)
+
+
+for i in r.json():
+    print i['name']
