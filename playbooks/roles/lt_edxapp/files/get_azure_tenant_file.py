17cbe69919b377139ea833abcf2db4f7c8556b14
yves <y.shen@learning-tribes.com>
2019-12-17 14:12:23 +0800

    add azure process

---
+++ playbooks/roles/lt_edxapp/files/get_azure_tenant_file.py
@@ -0,0 +1,21 @@
+from ConfigParser import SafeConfigParser
+import sys
+
+hosts_file = sys.argv[1]
+region = sys.argv[2]
+
+region_setion = str(region) + ':children'
+
+tenant_list = []
+parser = SafeConfigParser(allow_no_value=True)
+parser.optionxform = str
+parser.read(hosts_file)
+
+for i in parser.items(region_setion):
+    if 'stateful' not in i[0]:
+        tenant_name = i[0]
+        single_tenant_string = tenant_name
+        tenant_ip_list = parser.items(i[0])
+        for j in tenant_ip_list:
+            single_tenant_string = single_tenant_string + ',' + j[0]
+        print single_tenant_string
