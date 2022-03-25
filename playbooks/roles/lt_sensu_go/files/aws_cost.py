3a1a91ba13887236834a0952e40c5987075b77e3
yves <y.shen@learning-tribes.com>
2022-03-25 16:47:15 +0800

    aws cost script

--- playbooks/roles/lt_sensu_go/files/aws_cost.py
+++ playbooks/roles/lt_sensu_go/files/aws_cost.py
@@ -1,25 +1,61 @@
 import boto3
 import datetime
 import os
+import requests
 
+
+
+'''
 current_day = datetime.date.today()
 first_day_month = current_day - datetime.timedelta(days=current_day.day-1)
 yesterday_day = (current_day - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-print yesterday_day
+date_day = yesterday_day
 
 if current_day == first_day_month:
     os._exit(0)
 
 start_day = first_day_month.strftime("%Y-%m-%d")
 end_day = current_day.strftime("%Y-%m-%d")
-print start_day
-print end_day
+'''
 
+
+
+current_day = datetime.date.today()
+this_month_first_day = current_day.replace(day=1)
+last_month_last_day = this_month_first_day - datetime.timedelta(days=1)
+last_month_first_day = last_month_last_day.replace(day=1)
+start_day = last_month_first_day.strftime("%Y-%m-%d")
+end_day = this_month_first_day.strftime("%Y-%m-%d")
+date_day =last_month_last_day.strftime("%Y-%m-%d")
+
+
+
+def insert_data(name, client_name, type, country, month, service_name, product, cost):
+    url = 'https://devops-dashboard.triboolearning.com/api/awscost'
+    payload = {'name': name, 'client_name': client_name, 'type': type, 'country': country, 'month': month, 'service_name': service_name, 'product': product, 'cost': cost}
+    headers = {'Authorization': 'Token a5e3d87afc80a3848251919dc83303785a502063'}
+    response = requests.request("POST", url, headers=headers, data = payload)
+    print response.content
+
+def get_service_cost(service_name):
+    response_cost = client.get_cost_and_usage(
+        TimePeriod = {
+            'Start': start_day,
+            'End': end_day
+        },
+        Granularity = 'MONTHLY',
+        Metrics = ['BlendedCost'],
+        Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': [service_name]}}
+    )
+    service_cost = response_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
+    return round(float(service_cost), 2)
+
+def cost_add_tax(cost):
+    cost = cost * 1.2
+    return round(float(cost), 2)
 
 client = boto3.client('ce', region_name='us-east-1')
-
-
 
 response_tag = client.get_tags(
     TimePeriod = {
@@ -29,7 +65,6 @@ response_tag = client.get_tags(
     TagKey='cost-name'
     )
 tag_cost_list = response_tag['Tags']
-
 
 for i in tag_cost_list:
     response_tag_cost = client.get_cost_and_usage(
@@ -49,22 +84,10 @@ for i in tag_cost_list:
         if '' in i_list:
             pass
         else:
-            print str(i)+': '+str(cost)
+            cost = round(float(cost), 2)
+            cost = cost_add_tax(cost) 
+            insert_data(i, i_list[2], i_list[3], i_list[0], date_day, i_list[4], i_list[1], cost)
             break
-
-
-def get_service_cost(service_name):
-    response_cost = client.get_cost_and_usage(
-        TimePeriod = {
-            'Start': start_day,
-            'End': end_day
-        },
-        Granularity = 'MONTHLY',
-        Metrics = ['BlendedCost'],
-        Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': [service_name]}}
-    )
-    service_cost = response_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-    return round(float(service_cost), 2)
 
 cost_explorer_cost = get_service_cost('AWS Cost Explorer')
 waf_cost = get_service_cost('AWS WAF')
@@ -72,88 +95,16 @@ cloudtrail_cost = get_service_cost('AWS CloudTrail')
 cloudwatch_cost = get_service_cost('AmazonCloudWatch')
 kms_cost = get_service_cost('AWS Key Management Service')
 
-print cost_explorer_cost
-print waf_cost
-print cloudtrail_cost
-print cloudwatch_cost
-print kms_cost
+cost_explorer_cost = cost_add_tax(cost_explorer_cost)
+waf_cost = cost_add_tax(waf_cost)
+cloudtrail_cost = cost_add_tax(cloudtrail_cost)
+cloudwatch_cost = cost_add_tax(cloudwatch_cost)
+kms_cost = cost_add_tax(kms_cost)
 
+insert_data('GLOBAL-TRIBOO-NO_CLIENT-BILLING-API', 'NO_CLIENT', 'BILLING', 'GLOBAL', date_day, 'API', 'TRIBOO', cost_explorer_cost)
+insert_data('GLOBAL_EME-TRIBOO_2-SHARED-PRODUCTION-ELB', 'SHARED', 'PRODUCTION', 'GLOBAL_EME', date_day, 'ELB', 'TRIBOO_2', waf_cost)
+insert_data('GLOBAL-TRIBOO-NO_CLIENT-TOOL-CLOUDTRAIL', 'NO_CLIENT', 'TOOL', 'GLOBAL', date_day, 'CLOUDTRAIL', 'TRIBOO', cloudtrail_cost)
+insert_data('GLOBAL-TRIBOO-NO_CLIENT-TOOL-CLOUDWATCH', 'NO_CLIENT', 'TOOL', 'GLOBAL', date_day, 'CLOUDWATCH', 'TRIBOO', cloudwatch_cost)
+insert_data('GLOBAL-TRIBOO-NO_CLIENT-TOOL-KMS', 'NO_CLIENT', 'TOOL', 'GLOBAL', date_day, 'KMS', 'TRIBOO', kms_cost)
 
-
-
-'''
-response_ce_cost = client.get_cost_and_usage(
-    TimePeriod = {
-        'Start': start_day,
-        'End': end_day
-    },
-    Granularity = 'MONTHLY',
-    Metrics = ['BlendedCost'],
-    Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': ['AWS Cost Explorer']}}
-)
-ce_cost = response_ce_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-print ce_cost
-print float(ce_cost)
-print round(float(ce_cost), 2)
-
-
-response_waf_cost = client.get_cost_and_usage(
-    TimePeriod = {
-        'Start': start_day,
-        'End': end_day
-    },
-    Granularity = 'MONTHLY',
-    Metrics = ['BlendedCost'],
-    Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': ['AWS WAF']}}
-)
-waf_cost = response_waf_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-print waf_cost
-print float(waf_cost)
-print round(float(waf_cost), 2)
-
-
-response_ct_cost = client.get_cost_and_usage(
-    TimePeriod = {
-        'Start': start_day,
-        'End': end_day
-    },
-    Granularity = 'MONTHLY',
-    Metrics = ['BlendedCost'],
-    Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': ['AWS CloudTrail']}}
-)
-ct_cost = response_ct_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-print ct_cost
-print float(ct_cost)
-print round(float(ct_cost), 2)
-
-
-response_cw_cost = client.get_cost_and_usage(
-    TimePeriod = {
-        'Start': start_day,
-        'End': end_day
-    },
-    Granularity = 'MONTHLY',
-    Metrics = ['BlendedCost'],
-    Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': ['AmazonCloudWatch']}}
-)
-cw_cost = response_cw_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-print cw_cost
-print float(cw_cost)
-print round(float(cw_cost), 2)
-
-
-response_kms_cost = client.get_cost_and_usage(
-    TimePeriod = {
-        'Start': start_day,
-        'End': end_day
-    },
-    Granularity = 'MONTHLY',
-    Metrics = ['BlendedCost'],
-    Filter = {'Dimensions': {'Key': 'SERVICE', 'Values': ['AWS Key Management Service']}}
-)
-kms_cost = response_kms_cost['ResultsByTime'][0]['Total']['BlendedCost']['Amount']
-print kms_cost
-print float(kms_cost)
-print round(float(kms_cost), 2)
-'''
 
