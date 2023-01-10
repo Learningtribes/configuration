45cdbbbd67899ae8e61662e9c27b5543495b8e8e
yves <y.shen@learning-tribes.com>
2023-01-10 14:27:55 +0800

    update checkpoint for heartbeat API

--- playbooks/roles/lt_sensu_go/files/check_heartbeat_api.py
+++ playbooks/roles/lt_sensu_go/files/check_heartbeat_api.py
@@ -7,6 +7,7 @@ health_url = 'https://learning-tribes.com/heartbeat?extended'
 
 error_list = []
 request_timeout = False
+dns_error = False
 
 try:
     response = requests.get(health_url, timeout=20)
@@ -37,15 +38,35 @@ try:
                 error_list.append({'RabbitMQ': 'down'})
 except requests.exceptions.Timeout:
     request_timeout = True
+except requests.exceptions.ConnectionError:
+    dns_error = True
+except ValueError:
+    pass
 
+def devops_post(log):
+    devops_url = "https://devops-dashboard.triboolearning.com/api/downtimelog"
+    headers = {'Authorization': 'Token'}
+    instance_name = socket.gethostname()
+    try:
+        a = requests.post(devops_url, headers=headers, data={'client_hostname': instance_name, 'downtime_log': log})
+    except:
+        pass
 
 if request_timeout:
+    devops_post('platform timeout')
+    print(socket.gethostname()+'.health.status 0 '+str(int(time.time())))
+elif dns_error:
+    devops_post('platform dns error')
     print(socket.gethostname()+'.health.status 0 '+str(int(time.time())))
 elif response.status_code != 200:
+    devops_post('platform health API error')
     print(socket.gethostname()+'.health.status 0 '+str(int(time.time())))
 elif error_list != []:
+    for i in error_list:
+        service_name = i.keys()[0]
+        log_content = 'platform '+service_name.lower()+' error'
+        devops_post(log_content)
     print(socket.gethostname()+'.health.status 0 '+str(int(time.time())))
 else:
     print(socket.gethostname()+'.health.status 100 '+str(int(time.time())))
-
 
