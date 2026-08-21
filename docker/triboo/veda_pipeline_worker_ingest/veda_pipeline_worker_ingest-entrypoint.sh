27d626acc818fe7e769b22c96ae210cb1a8032f4
yves <y.shen@learning-tribes.com>
2026-08-21 14:37:21 +0800

    veda_pipeline_worker_ingest Dockerfile

---
+++ docker/triboo/veda_pipeline_worker_ingest/veda_pipeline_worker_ingest-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -f /edx/app/veda_pipeline_worker/veda_pipeline_worker_env ]; then
+    source /edx/app/veda_pipeline_worker/veda_pipeline_worker_env
+fi
+
+exec python /edx/app/veda_pipeline_worker/veda_pipeline_worker/bin/ingest
